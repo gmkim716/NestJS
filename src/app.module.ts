1b50849001296c6fbfa02b4216a11140d2146e9c
Gyeongmin Kim <gmkim716@gmail.com>
2025-03-30 12:14:26 +0900

    feat: 환경변수 파일을 사용한다

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,19 @@
 import { Module } from '@nestjs/common';
 import { MovieModule } from './movie/movie.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 // IoC 컨테이너에 관리될 클래스들을 모아두는 곳
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'postgres',
+      type: process.env.DB_TYPE as 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? '5432'),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [],
       synchronize: true, // 개발할 때만 true 적용, 자동으로 코드와 DB를 동기화 적용
     }),
